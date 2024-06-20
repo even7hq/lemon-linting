9b757ff88dc01be5574add59cfc1fbcd64980f98
Matheus Giovani <teusgiovani@gmail.com>
2024-06-20 16:20:04 -0300

    fix: parserOptions pede projeto

--- typescript.js
+++ typescript.js
@@ -1,57 +1,66 @@
 module.exports = {
     root: true,
+    overrides: [
+        {
+            files: ["**/*.ts"],
+            extends: [
+                "plugin:@typescript-eslint/recommended"
+            ],
 
-    extends: [
-        "plugin:@typescript-eslint/recommended"
-    ],
+            parser: "@typescript-eslint/parser",
 
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-
-    rules: {
-        // Allow explicit any
-        "@typescript-eslint/no-explicit-any": "off",
-
-        // Allow TS comments
-        "@typescript-eslint/ban-ts-comment": "off",
-
-        // This conflicts with "keyword-spacing"
-        "@typescript-eslint/keyword-spacing": "off",
-
-        // Allow namespaces
-        "@typescript-eslint/no-namespace": "off",
-
-        // Enforce brace style
-        "curly": "error",
-
-        // Deny using multiple empty lines
-        "no-multiple-empty-lines": ["warn", {
-            "max": 1
-        }],
-
-        // Enforce using camel case
-        "@typescript-eslint/naming-convention": ["warn",
-            {
-                "selector": "variableLike",
-                "format": ["camelCase", "PascalCase"]
+            parserOptions: {
+                project: true
             },
-            {
-                "selector": "parameter",
-                "format": ["camelCase"]
-            },
-            {
-                "selector": ["classProperty", "classMethod"],
-                "format": ["camelCase", "snake_case"]
-            },
-            // Enforce enums to be uppercase
-            {
-                "selector": "enumMember",
-                "format": ["UPPER_CASE"]
-            },
-            {
-                "selector": "typeLike",
-                "format": ["PascalCase"]
-            },
-        ]
-    }
+
+            plugins: ["@typescript-eslint"],
+
+            rules: {
+                // Allow explicit any
+                "@typescript-eslint/no-explicit-any": "off",
+
+                // Allow TS comments
+                "@typescript-eslint/ban-ts-comment": "off",
+
+                // This conflicts with "keyword-spacing"
+                "@typescript-eslint/keyword-spacing": "off",
+
+                // Allow namespaces
+                "@typescript-eslint/no-namespace": "off",
+
+                // Enforce brace style
+                "curly": "error",
+
+                // Deny using multiple empty lines
+                "no-multiple-empty-lines": ["warn", {
+                    "max": 1
+                }],
+
+                // Enforce using camel case
+                "@typescript-eslint/naming-convention": ["warn",
+                    {
+                        "selector": "variableLike",
+                        "format": ["camelCase", "PascalCase"]
+                    },
+                    {
+                        "selector": "parameter",
+                        "format": ["camelCase"]
+                    },
+                    {
+                        "selector": ["classProperty", "classMethod"],
+                        "format": ["camelCase", "snake_case"]
+                    },
+                    // Enforce enums to be uppercase
+                    {
+                        "selector": "enumMember",
+                        "format": ["UPPER_CASE"]
+                    },
+                    {
+                        "selector": "typeLike",
+                        "format": ["PascalCase"]
+                    },
+                ]
+            }
+        }
+    ]
 }
